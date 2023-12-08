2b9ea32d42c56c8ec7938b7a7afa2eefc6c2eb18
anandbaraik <anandbaraik2014@gmail.com>
2023-12-08 23:40:16 +0530

    solve: q2 anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,37 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length !== str2.length) return false;
+  let map = {};
+  addCharFrquency(str1.toLowerCase(), map);
+  subtractCharFrquency(str2.toLowerCase(), map);
+  return checkCharFrequency(map);
 }
 
+function addCharFrquency(s, map) {
+  for (let char of s) {
+    if (!map[char]) {
+      map[char] = 1;
+    } else {
+      map[char] = map[char] + 1;
+    }
+  }
+}
+
+function subtractCharFrquency(t, map) {
+  for (let char of t) {
+    if (!map[char]) continue;
+
+    if (map[char]) {
+      map[char] = map[char] - 1;
+    }
+  }
+}
+
+function checkCharFrequency(map) {
+  for (let char in map) {
+    if (!(map[char] == 0)) return false;
+  }
+  return true;
+}
 module.exports = isAnagram;
